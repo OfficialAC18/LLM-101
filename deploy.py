cf0b27076c4fb85dc887daa63b0347d105c884be
Akchunya Chanchal <akchunyachanchal@gmail.com>
2024-06-01 14:09:55 +0100

    Final Updates to Repo, Proper refactoring TBA

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ from pyngrok import ngrok
 from threading import Thread
 
 #Set Ngrok token
-ngrok.set_auth_token("Enter Token Here")
+ngrok.set_auth_token("2eFqHIK3WMZdd1P7R0ZkWI4dxNI_4dtGYa9jiqHY85JW3F4PX")
 
 #Open Tunnel to port 
 url = ngrok.connect(addr = 8501,
@@ -11,7 +11,7 @@ url = ngrok.connect(addr = 8501,
                     bind_tls=True)
 
 def run_streamlit():
-    os.system('streamlit run "Full path to app.py" --server.port 8501')
+    os.system('streamlit run "/scratch/users/k23058970/LLM-101/app.py" --server.port 8501')
 
 print("App is live at:",url)
 
